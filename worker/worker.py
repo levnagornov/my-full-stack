5e73cfb9b4d20c73a38ebb96fb0ea7d026aa3523
Lev Nagornov <levnagornov@yahoo.com>
2025-10-27 22:12:44 +0100

    update worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,9 +1,11 @@
 from celery import Celery
 import redis
 import json
-import os
+from datetime import datetime
+import time
 
 from config import settings
+
 
 celery_app = Celery("app.tasks", broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_RESULT_BACKEND)
 
@@ -17,12 +19,8 @@ celery_app.conf.update(
 r = redis.from_url(settings.REDIS_URL, decode_responses=True)
 
 @celery_app.task(bind=True)
-def dry_run(self, order_id: str, duration: int = 5):
-    tid = self.request.id
-    r.publish("celery_updates", json.dumps({"task_id": tid, "status": "STARTED", "order_id": order_id}))
-    import time
-    for i in range(duration):
-        time.sleep(1)
-        r.publish("celery_updates", json.dumps({"task_id": tid, "status": f"PROGRESS {(i+1)/duration:.2%}", "order_id": order_id}))
-    r.publish("celery_updates", json.dumps({"task_id": tid, "status": "SUCCESS", "order_id": order_id}))
-    return {"order_id": order_id, "status": "done"}
+def start_recalculation(self, task_id: int, reporting_date: datetime, systems: list[str]):
+    r.publish("celery_updates", json.dumps({"task_id": task_id, "status": "STARTED"}))
+    time.sleep(5)
+    r.publish("celery_updates", json.dumps({"task_id": task_id, "status": "SUCCESS"}))
+    return {"task_id": task_id, "status": "SUCCESS"}
